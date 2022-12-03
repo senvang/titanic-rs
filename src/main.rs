c6427c4944988d9b4607e60e4edd5080d854c8f8
Stefan Römer <sroemer@github.com>
2022-12-03 12:43:00 +0700

    initial cli added (subcommands 'start' and 'stop')

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// start work on a project and track your working times
+    Start {
+        #[arg(long)]
+        customer: String,
+
+        #[arg(long)]
+        project: String,
+
+        #[arg(long)]
+        comment: Option<String>,
+    },
+    /// stop working on a project
+    Stop {
+    },
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let _cli = Cli::parse();
 }
